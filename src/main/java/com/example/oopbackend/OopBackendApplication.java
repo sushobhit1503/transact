910fe23a070da1777a639d5bcee1fac967eb30b6
Sushobhit Srivastava <sushobhitofficial@gmail.com>
2022-04-07 00:30:35 +0530

    First Commit

---
+++ src/main/java/com/example/oopbackend/OopBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.oopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopBackendApplication.class, args);
+		System.out.println("Hello and welcome to Spring Boot project");
+	}
+
+}
---
+++ src/test/java/com/example/oopbackend/OopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
